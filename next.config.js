c0ca2b3e48838ce0baca8d68e8ee17b3667ef09c
huang.nicolas <huang.nicolas3wa.io>
2025-12-16 18:32:44 +0100

    fix: add webpack alias for @ to app directory (vercel)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   experimental: {
     serverActions: true,
+  },
+
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "app"),
+    };
+    return config;
   },
 };
 
