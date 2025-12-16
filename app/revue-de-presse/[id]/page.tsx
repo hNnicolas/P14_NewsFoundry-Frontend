eb23481344c0e2ef2c61281336b6637ca9a2efc6
huang.nicolas <huang.nicolas3wa.io>
2025-12-17 00:28:09 +0100

    fix: SideBar import casing

--- app/revue-de-presse/[id]/page.tsx
+++ app/revue-de-presse/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState, useRef } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import SideBar from "@/components/Sidebar";
+import SideBar from "@/components/SideBar";
 import Image from "next/image";
 
 type Article = {
